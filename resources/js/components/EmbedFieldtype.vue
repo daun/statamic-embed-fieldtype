c0b064d89b039c7396138e7a83e7bfa5fcd4b41b
Philipp Daun <post@philippdaun.net>
2025-12-04 19:36:12 +0100

    Make url input prefix configurable
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/js/components/EmbedFieldtype.vue
+++ resources/js/components/EmbedFieldtype.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col space-y-2 p-2 bg-gray-50 border border-gray-300 dark:bg-transparent dark:border-gray-700 rounded-xl">
         <ui-input-group>
-            <ui-input-group-prepend :text="__('URL')" />
+            <ui-input-group-prepend v-if="prepend" :text="__(prepend)" />
             <ui-input
                 :model-value="value"
                 :isReadOnly="isReadOnly"
@@ -10,7 +10,7 @@
                 @update:model-value="update"
                 @focus="$emit('focus')"
                 @blur="$emit('blur')"
-                input-class="border-s-0"
+                :input-class="prepend ? 'border-s-0' : ''"
             />
         </ui-input-group>
         <article
@@ -68,6 +68,14 @@ export default {
         };
     },
     computed: {
+        prepend() {
+            const { prepend = 'URL' } = this.config;
+            if (['null', 'false'].includes(prepend)) {
+                return null;
+            } else {
+                return prepend;
+            }
+        },
         info() {
             return this.embedInfo[this.value] || {};
         },
